863f85096a5cefade8a55775fa9f4e18a6fe7756
Gabriel Gil <gabrielgilbc@gmail.com>
2022-05-20 23:03:09 -0300

    iniciando o GIT

---
+++ public/js/index.js
@@ -0,0 +1,100 @@
+const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged")
+const session = localStorage.getItem("session")
+
+checkLogged();
+
+//LOGAR NO SISTEMA
+document.getElementById("login-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  const email = document.getElementById("email-input").value;
+  const password = document.getElementById("password-input").value;
+  const chckSession = document.getElementById("session-check").checked;
+
+  const account = getAccount(email)
+
+  if(!account) {
+    alert("Opss! Verifique o usuário ou a senha.");
+    return;
+  }
+
+  if(account) {
+      if(account.password !== password){
+        alert("Opss! Verifique o usuário ou a senha.");
+         return;
+      }
+
+      saveSession(email, chckSession);
+
+      window.location.href = "home.html";
+  }
+});
+
+//CRIAR CONTA
+document.getElementById("create-form").addEventListener("submit",function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    console.log (email, password);
+
+  if(email.length < 5){
+    alert("Preencha o campo com um e-mail válido")
+    return;
+
+  }
+
+  if(password.length <4) {
+    alert("Preenha a senha com no mínimo 4 digitos")
+    return;
+  }
+
+  saveAccout({
+    login: email,
+    password: password,
+    trasactions: []
+  });
+
+
+  myModal.hide();
+
+  alert("Conta criada com suceso.")
+})
+
+function checkLogged() {
+  if(session){
+    sessionStorage.setItem("logged", session);
+    logged = session;
+  }
+
+  if(logged){
+    saveSession(logged, session);
+
+    window.location.href = "home.html"
+  }
+}
+
+function saveAccout(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSession(data, saveSession) {
+    if(saveSession){
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
+
+function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+  if(account) {
+    return JSON.parse(account);
+  }
+
+  return "";
+}
+
